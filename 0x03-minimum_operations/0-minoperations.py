3e4ecabb31f7ca445949bb249d6ba32a53d17015
Myduzo <sahli.youssef@outlook.com>
2021-01-05 17:48:02 +0100

    minOperations function

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Minimum Operations function
+"""
+
+
+def minOperations(n):
+    hlist = ['H']
+    flag = 1
+
+    if n is None:
+        return 0
+
+    if (n % 3) == 0:
+        while (len(hlist) != n):
+            if (len(hlist) == n / 3):
+                copylist = hlist[:]
+                flag += 1
+            if (len(hlist) >= n / 3):
+                hlist.extend(copylist)
+                flag += 1
+            else:
+                hlist.append('H')
+                flag += 1
+        return flag
+
+    if (n % 2) == 0:
+        while (len(hlist) != n):
+            if (len(hlist) == n / 2):
+                copylist = hlist[:]
+                flag += 1
+            if (len(hlist) >= n / 2):
+                hlist.extend(copylist)
+                flag += 1
+            else:
+                hlist.append('H')
+                flag += 1
+        return flag
+
+    else:
+        while (len(hlist) != n):
+            hlist.append('H')
+            flag += 1
+        return flag
