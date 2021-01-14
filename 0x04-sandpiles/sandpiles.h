d04e8fe4554c61fd8bafb264749cd4a9196467f0
Myduzo <sahli.youssef@outlook.com>
2021-01-14 18:16:56 +0100

    library

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static void sp_print_grid(int grid[3][3]);
-void sp_sum(int grid1[3][3], int grid2[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+int sp_check(int grid1[3][3]);
+void sp_stable (int grid1[3][3], int test[3][3]);
+void sp_print_grid(int grid[3][3]);
 
 #endif /* _SANDPILES_H_ */
