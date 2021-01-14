56a46f36e8e57baeb92f68cc2c7a024ba7d11089
Myduzo <sahli.youssef@outlook.com>
2021-01-14 16:50:08 +0100

    sandpiles

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,45 @@
+#include "sandpiles.h"
+/**
+ * sp_sum - function that so the sum of 2 sandpiels
+ * @grid1: sandpiles 1
+ * @grid2: sandpiles 2
+ */
+void sp_sum(int grid1[3][3], int grid2[3][3])
+{
+  int i = 0, j = 0;
+  int gridsum[3][3];
+
+  while (i < 3)
+  {
+    while (j < 3)
+    {
+      gridsum[i][j] = grid1[i][j] + grid2[i][j];
+      j++;
+    }
+    i++;
+  }
+}
+
+
+
+/**
+ * my_print_grid - Print 3x3 grid
+ * @grid: 3x3 grid
+ *
+ */
+static void my_print_grid(int grid[3][3])
+{
+    int i, j;
+
+    printf("=\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,10 @@
+#ifndef _SANDPILES_H_
+#define _SANDPILES_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static void my_print_grid(int grid[3][3]);
+void sp_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif /* _SANDPILES_H_ */
