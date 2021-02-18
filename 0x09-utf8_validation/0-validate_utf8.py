39cebc0e7c4450d378be55cac060d49cefb26ee5
Myduzo <sahli.youssef@outlook.com>
2021-02-18 17:27:42 +0100

    fixed

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -3,13 +3,20 @@
 Method that determines if a given data represents a valid UTF-8 encoding
 """
 
+
 def validUTF8(data):
     """
     Valid UTF-8 function
     """
-
+    arr = []
     for idx in data:
-        if idx > 127:
-            return False
-        
+        if idx < 255:
+            arr.append(idx)
+
+    nbytes = bytes(arr)
+    try:
+        nbytes.decode()
+    except UnicodeError:
+        return False
+
     return True
