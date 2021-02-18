ea0f0f3c57c3bd0ea379b9ed050753bab686d96a
Myduzo <sahli.youssef@outlook.com>
2021-02-18 18:13:59 +0100

    fixed

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -8,16 +8,25 @@ def validUTF8(data):
     """
     Valid UTF-8 function
     """
-    arr = []
-    for idx in data:
-        if idx <= 255:
-            arr.append(idx)
-        else:
-            arr.append(0)
+    # arr = []
+    # for idx in data:
+    #     if idx <= 255:
+    #         arr.append(idx)
+    #     else:
+    #         arr.append(0)
 
-    nbytes = bytes(arr)
+    # nbytes = bytes(arr)
+    # try:
+    #     nbytes.decode()
+    # except UnicodeDecodeError:
+    #     return False
+
+    # return True
+
+    refdat = [x & 0b11111111 for x in data]
+    bytedat = bytes(refdat)
     try:
-        nbytes.decode()
+        bytedat.decode()
     except UnicodeDecodeError:
         return False
 
