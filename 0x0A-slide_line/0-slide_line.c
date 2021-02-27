ae555166250db90cb1479c60a491204a000b7123
Myduzo <sahli.youssef@outlook.com>
2021-02-27 19:11:22 +0100

    slide line

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,7 +1,7 @@
 #include "slide_line.h"
 
 /**
- * slide_line - slides a one-dimensional 2048 game
+ * slide_line - function that slides and merges an array of integers
  *
  * @line: Pointer to the first int in the line
  * @size: The size of line
@@ -11,13 +11,44 @@
  **/
 int slide_line(int *line, size_t size, int direction)
 {
-    
+    int *place_here;
+    int *left;
+    int *right;
+
     if (!line)
         return(0);
 
-    if (direction != SLIDE_LEFT && direction !=SLIDE_RIGHT)
+    if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
         return(0);
-    
-    
 
+    if (direction == SLIDE_LEFT) {
+        place_here = line;
+        left = place_here;
+
+        while (left < line + (size - 1)) {
+            while (*left == 0) {
+                left++;
+            }
+            right = left + 1;
+            while (right < line + (size)) {
+                if (*right == *left) {
+                    *place_here = *left * 2;
+                    if (place_here != left) {
+                        *left = 0;
+                    }
+                    *right = 0;
+                    place_here++;
+                    break;
+                    } else {
+                    right++;
+                }
+            }
+            left++;
+        }
+        if (*(line + size - 1) && !*place_here) {
+            *place_here = *(line + size - 1);
+            *(line + size - 1) = 0;
+        }
+    }
+    return (1);
 }
