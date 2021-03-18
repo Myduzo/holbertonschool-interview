b9c655c569994203b3130c15535c801941e8212a
Myduzo <sahli.youssef@outlook.com>
2021-03-18 20:35:00 +0100

    nqueens

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""0. N queens
+"""
+import sys
+
+
+def checkQueen(board, row, col):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def nqueens(N):
+    return result
+
+
+def main():
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
+        exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        exit(1)
+
+    if n < 4:
+        print('N must be at least 4')
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()
