77c973d0271480f4da16b959b23c21cd50ac93c2
Myduzo <sahli.youssef@outlook.com>
2021-07-04 18:41:58 +0100

    betty fixed

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -1,68 +1,74 @@
 #include "list.h"
 
 /**
- * 
- * 
- * 
+ * add_node_end - function that adds node at the end
+ *
+ * @list: list to modify
+ * @str: the string to copy into the new node
+ *
+ * Return: Address of the new node, or NULL on failure.
  */
 List *add_node_end(List **list, char *str)
 {
-  List *node = malloc(sizeof(List));
-  
-  if (!node)
-    return (NULL);
+	List *node = malloc(sizeof(List));
 
-  node->str = str;
-  if (!node->str)
-    free(node);
+	if (!node)
+		return (NULL);
 
-  if(!(*list))
-  {
-    node->next = node;
-    node->prev = node;
-    *list = node;
-  }
+	node->str = str;
+	if (!node->str)
+		free(node);
 
-  else
-  {
-    node->next = (*list);
-    node->prev = (*list)->prev;
-    (*list)->prev->next = node;
-    (*list)->prev = node;
-  }
-  return (node);
+	if (!(*list))
+	{
+		node->next = node;
+		node->prev = node;
+		*list = node;
+	}
+
+	else
+	{
+		node->next = (*list);
+		node->prev = (*list)->prev;
+		(*list)->prev->next = node;
+		(*list)->prev = node;
+	}
+	return (node);
 }
 
 /**
- * 
- * 
- * 
+ * add_node_begin - function that adds node at the beginning
+ *
+ * @list: list to modify
+ * @str: the string to copy into the new node
+ *
+ * Return: Address of the new node, or NULL on failure.
  */
 List *add_node_begin(List **list, char *str)
 {
-  List *node = malloc(sizeof(List));
-  
-  if (!node)
-    return (NULL);
+	List *node = malloc(sizeof(List));
 
-  node->str = str;
-  if (!node->str)
-    free(node);
+	if (!node)
+		return (NULL);
 
-  if(!(*list))
-  {
-    node->next = node;
-    node->prev = node;
-    *list = node;
-  }
+	node->str = str;
+	if (!node->str)
+		free(node);
 
-  else
-  {
-    node->next = (*list);
-    node->prev = (*list)->prev;
-    (*list)->prev->next = node;
-    (*list)->prev = node;
-    *list = node;
-  }
-  return (node);
+	if (!(*list))
+	{
+		node->next = node;
+		node->prev = node;
+		*list = node;
+	}
+
+	else
+	{
+		node->next = (*list);
+		node->prev = (*list)->prev;
+		(*list)->prev->next = node;
+		(*list)->prev = node;
+		*list = node;
+	}
+	return (node);
 }
