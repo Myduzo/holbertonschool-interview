55b8e42cafc1ea298f76a0ddff3386a69ffca73a
Myduzo <sahli.youssef@outlook.com>
2021-07-14 23:05:10 +0100

    island perimeter

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -8,18 +8,15 @@ def island_perimeter(grid):
     """
     count = 0
 
-    if len(grid) > 100:
-        return None
-
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i][j-1] == 0 or grid[i][j-1] is None:
+                if grid[i][j-1] == 0:
                     count += 1
-                if grid[i-1][j] == 0 or grid[i][j-1] is None:
+                if grid[i-1][j] == 0:
                     count += 1
-                if grid[i][j+1] == 0 or grid[i][j-1] is None:
+                if grid[i][j+1] == 0:
                     count += 1
-                if grid[i+1][j] == 0 or grid[i][j-1] is None:
+                if grid[i+1][j] == 0:
                     count += 1
     return count
