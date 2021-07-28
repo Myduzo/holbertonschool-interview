682d81202b27ed8f0eceb46cedbdc69b439a865f
Myduzo <sahli.youssef@outlook.com>
2021-07-28 16:09:06 +0100

    wildcmp

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * wildcmp - function that compares two strings
+ *
+ * @s1: string pointer
+ * @s2: string pointer
+ *
+ * Return: 1 if the strings identical, otherwise return 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1)
+	{
+		if (*s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (!*s2);
+	}
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+
+	return (0);
+}
---
+++ 0x1E-wild_cmp/holberton.h
@@ -0,0 +1,8 @@
+#ifndef _WILD_COMPARE_
+#define _WILD_COMPARE_
+
+#include <stdio.h>
+
+int wildcmp(char *s1, char *s2);
+
+#endif
